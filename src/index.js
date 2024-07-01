194b2820b00c4ac0ccf3cde18288340343928c2d
Karocapo <55492707+Karocapo@users.noreply.github.com>
2024-07-01 17:02:46 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.karocapo.com": dockerHub,
+  "quay.karocapo.com": "https://quay.io",
+  "gcr.karocapo.com": "https://gcr.io",
+  "k8s-gcr.karocapo.com": "https://k8s.gcr.io",
+  "k8s.karocapo.com": "https://registry.k8s.io",
+  "ghcr.karocapo.com": "https://ghcr.io",
+  "cloudsmith.karocapo.com": "https://docker.cloudsmith.io",
+  "ecr.karocapo.com": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.karocapo.com": dockerHub,
 };
 
 function routeByHosts(host) {
